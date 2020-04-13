9829dc470cd5bfbf674df23c43861e0c92b907e8
Carlos Pinochet <cpinochet@nisum.com>
2020-04-12 20:26:22 -0400

    Adding variables

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   version = "3.5.0"
 
-  credentials = file("../Documents/My TERRA Project 9323-dfb7cadc4e50.json")
+  credentials = file(var.credentials_file)
 
-  project = "my-terra-project-9323"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
